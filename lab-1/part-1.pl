b869df34d62a3c34f1faeb0b6766829013e74830
Sergey Levko <okvel.s@gmail.com>
2019-04-17 21:57:49 +0300

    Implement part 1 and fix part 2

--- lab-1/part-1.pl
+++ lab-1/part-1.pl
@@ -1,10 +1,27 @@
-bigger(X, Y) :-
-    X > Y.
+:- dynamic bigger/2.
+:- dynamic lower/2.
 
-start :-
-    repeat,
-    writeln('Enter clause'),
-    read(X),
-    %term_to_atom(Term, bigger),
-    %writeln(term(Term)),
-    X = 'end' -> fail.
+bigger('car', 'motorcycle').
+lower('car', 'jeep').
+lower('jeep', 'van').
+
+
+findLower([X],X).
+findLower([X|T],X) :-
+    findLower(T,Y),
+    lower(Y,X).
+findLower([_|T],Y) :-
+    findLower(T,Y).
+theMostLower :-
+    findall(X, (lower(X,Y);bigger(Y,X)), L) -> findLower(L,M),
+    format('~w is the most lower object.', M).
+
+findBigger([X],X).
+findBigger([X|T],X) :-
+    findLower(T,Y),
+    bigger(Y,X).
+findBigger([_|T],Y) :-
+    findLower(T,Y).
+theMostBigger :-
+    findall(X, (bigger(X,Y);lower(Y,X)), L) -> findBigger(L,M),
+    format('~w is the most bigger object.', M).
