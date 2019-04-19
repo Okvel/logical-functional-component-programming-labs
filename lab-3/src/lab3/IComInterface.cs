372ad289941b5be5cba523bec11af5fefc265ed4
Sergey Levko <okvel.s@gmail.com>
2019-04-19 17:36:00 +0300

    Implement lab 3

---
+++ lab-3/src/lab3/IComInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace lab3
+{
+    [ComVisible(true)]
+    [Guid("2657A5E6-D2C9-47D1-8A00-41B17C2F3B63")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface IComInterface
+    {
+        float Sum(float a, float b);
+        float Sub(float a, float b);
+        float Mul(float a, float b);
+        float Div(float a, float b);
+    }
+}
