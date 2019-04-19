9539a85fde55c7456d29deda37fb09c7908ea2f5
Sergey <Okvel@users.noreply.github.com>
2019-04-19 17:40:03 +0300

    Implement lab 3 (#4)

---
+++ lab-3/src/lab3/ComClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace lab3
+{
+    [ComVisible(true)]
+    [Guid("6BF23DAE-D5E2-48AA-8815-BB4F451CF91B")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class ComClass : IComInterface
+    {
+        public ComClass() { }
+
+        public float Sum(float a, float b)
+        {
+            return a + b;
+        }
+
+        public float Sub(float a, float b)
+        {
+            return a - b;
+        }
+
+        public float Mul(float a, float b)
+        {
+            return a * b;
+        }
+
+        public float Div(float a, float b)
+        {
+            return a / b;
+        }
+    }
+}
